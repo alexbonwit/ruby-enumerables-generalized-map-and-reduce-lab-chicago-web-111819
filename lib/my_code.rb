98c43de88b39a93ab7eca6a37f1afb006ed6c57a
Alex Bonwit <alexbonwit@mac.com>
2019-11-15 02:27:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,38 @@
 # Your Code Here
+
+def map(source)
+  new = []
+  i = 0
+  
+  while i < source.length do
+    new << yield(source[i])
+    i += 1
+  end  
+  new
+end
+
+
+def reduce(source, start = 0)
+  i = 0
+  value = 0
+  while i < source.length do
+    yield(source[i])
+    i += 1
+  end
+  value
+end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
