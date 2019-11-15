7c37330f15a42b66978961c1cffe98d541fcfba7
Alex Bonwit <alexbonwit@mac.com>
2019-11-15 02:48:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,9 +15,9 @@ end
 
 def reduce(source, start = 0)
   i = 0
-  value = 0
+  # value = 0
   while i < source.length do
-    value = yield(value, source[i])
+    start = yield(start, source[i])
     i += 1
   end
   value
